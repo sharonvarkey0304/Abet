b896c2e9cc0642d73300909596ad30fbced5029c
Abhinav <abhinavpm91@gmail.com>
2024-02-19 23:03:44 +0530

    added add product feature in the ui

--- lib/screens/sidebar/store/add2.dart
+++ lib/screens/sidebar/store/add2.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:loginpage/controler/store_controller.dart';
+import 'package:loginpage/widgets/snackbar.dart';
 
 class AddProduct extends StatefulWidget {
   const AddProduct({Key? key}) : super(key: key);
@@ -9,6 +14,9 @@ class AddProduct extends StatefulWidget {
 }
 
 class _AddProductState extends State<AddProduct> {
+  final _storeController = Get.find<StoreController>();
+  GlobalKey<FormState> productFormKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,113 +47,158 @@ class _AddProductState extends State<AddProduct> {
             ),
             Expanded(
               child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 30, right: 25, left: 25),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Product name',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 25),
-                      child: TextFormField(
-                        maxLines: 4,
-                        keyboardType: TextInputType.multiline,
-                        decoration: InputDecoration(
-                          labelText: 'Details',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 30, right: 25, left: 25),
-                      child: TextFormField(
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          labelText: 'Price',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 30, right: 25, left: 25),
-                      child: TextFormField(
-                        keyboardType: TextInputType.phone,
-                        decoration: InputDecoration(
-                          labelText: 'Contact number',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 30, right: 25, left: 25),
-                      child: TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: Stack(
-                        children: [
-                          //image container
-                          SizedBox(
-                            height: 190,
-                            width: 170,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(30),
-                              child: Image.asset(
-                                'assets/images/nophoto.png',
-                                fit: BoxFit.cover,
+                child: Form(
+                  key: productFormKey,
+                  child: GetBuilder<StoreController>(builder: (controller) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding:
+                              EdgeInsets.only(top: 30, right: 25, left: 25),
+                          child: TextFormField(
+                            controller: _storeController.productNameController,
+                            validator: _storeController.commonValidator,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            decoration: InputDecoration(
+                              labelText: 'Product name',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
                               ),
                             ),
                           ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 25),
+                          child: TextFormField(
+                            controller:
+                                _storeController.productDetailsController,
+                            validator: _storeController.commonValidator,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            maxLines: 4,
+                            keyboardType: TextInputType.multiline,
+                            decoration: InputDecoration(
+                              labelText: 'Details',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Padding(
+                          padding:
+                              EdgeInsets.only(top: 30, right: 25, left: 25),
+                          child: TextFormField(
+                            controller: _storeController.productPriceController,
+                            validator: _storeController.commonValidator,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Price',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              EdgeInsets.only(top: 30, right: 25, left: 25),
+                          child: TextFormField(
+                            controller:
+                                _storeController.productContactNumController,
+                            validator: _storeController.commonValidator,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            keyboardType: TextInputType.phone,
+                            decoration: InputDecoration(
+                              labelText: 'Contact number',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              EdgeInsets.only(top: 30, right: 25, left: 25),
+                          child: TextFormField(
+                            controller: _storeController.productEmailController,
+                            validator: _storeController.commonValidator,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Center(
+                          child: Stack(
+                            children: [
+                              //image container
+                              SizedBox(
+                                height: 190,
+                                width: 170,
+                                child: controller.image != null
+                                    ? Image.file(
+                                        height: 100,
+                                        width: 100,
+                                        fit: BoxFit.cover,
+                                        File(controller.image ?? ''),
+                                      )
+                                    : ClipRRect(
+                                        borderRadius: BorderRadius.circular(30),
+                                        child: Image.asset(
+                                          'assets/images/nophoto.png',
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                              ),
 
-                          //image add icon
-                          Positioned(
-                            top: 140,
-                            left: 120,
-                            child: IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                FontAwesomeIcons.cameraRetro,
+                              //image add icon
+                              Positioned(
+                                top: 140,
+                                left: 120,
+                                child: IconButton(
+                                  onPressed: controller.pickImage,
+                                  icon: const Icon(
+                                    FontAwesomeIcons.cameraRetro,
+                                  ),
+                                ),
                               ),
-                            ),
+                            ],
                           ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Submit'),
-                    ),
-                  ],
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            await _storeController
+                                .addProductBtnOntap(productFormKey);
+                          },
+                          child: controller.addProductStatus ==
+                                  AddProductStatus.loading
+                              ? CommonWidget.loadingIndicator()
+                              : const Text('Submit'),
+                        ),
+                      ],
+                    );
+                  }),
                 ),
               ),
             ),
