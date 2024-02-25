2f3409204a851d954cfe3ff9a355b1cc51de1b48
siddique <siddiquekp240@gmail.com>
2024-02-25 19:03:05 +0530

    Your uploads completed

--- lib/screens/userscreen/profile/youruploads.dart
+++ lib/screens/userscreen/profile/youruploads.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:loginpage/model/contribution_model.dart';
+import 'package:loginpage/widgets/snackbar.dart';
+import '../../../controler/contribution_controller.dart';
 
 class Uploads extends StatefulWidget {
   const Uploads({super.key});
@@ -9,44 +14,84 @@ class Uploads extends StatefulWidget {
 }
 
 class _UploadsState extends State<Uploads> {
+  final contributionController = Get.put(ContributionController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView(
-        children: [
-          SizedBox(
-            height: 330,
-            width: MediaQuery.of(context).size.width,
-            child: Image.asset(
-              'assets/images/upload.jpg',
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
+      appBar: AppBar(
+        surfaceTintColor: Colors.white,
+      ),
+      body: GetBuilder<ContributionController>(
+        builder: (controller) {
+          log("your upload ${controller.yourUploadSubjectList.length}");
+          if (controller.contributionStatus == ContributionStatus.loading) {
+            return Center(
+              child: CommonWidget.loadingIndicator(color: Colors.amber),
+            );
+          }
+          return ListView(
+            children: [
+              SizedBox(
+                height: 330,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/upload.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
                   padding: EdgeInsets.only(left: 9),
                   child: Text(
                     "Recent Uploads",
                     style: GoogleFonts.poppins(fontSize: 30),
-                  ))),
-          SizedBox(height: 30),
-          _buildUploadItem(
-              fileName: 'html module 3', directory: 'semester 1/html'),
-          SizedBox(height: 20),
-          _buildUploadItem(
-              fileName: 'java module 1', directory: 'semester 5/java'),
-        ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              controller.yourUploadSubjectList.isEmpty
+                  ? Center(
+                      child: Text("NO UPLOADS"),
+                    )
+                  : ListView.separated(
+                      shrinkWrap: true,
+                      physics: ScrollPhysics(),
+                      itemCount: controller.yourUploadSubjectList.length,
+                      itemBuilder: (context, index) {
+                        final subject = controller.yourUploadSubjectList[index];
+
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: _buildUploadItem(
+                              fileName: subject.title ?? "NO NAME",
+                              directory: '',
+                              item: subject),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return SizedBox(
+                          height: 15,
+                        );
+                      },
+                    ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
 
   Widget _buildUploadItem(
-      {required String fileName, required String directory}) {
+      {required String fileName,
+      required String directory,
+      required Subject item}) {
     return Container(
       padding: EdgeInsets.all(8),
       margin: EdgeInsets.symmetric(vertical: 4),
@@ -91,7 +136,7 @@ class _UploadsState extends State<Uploads> {
           Spacer(), // Adds space to push the next icon to the end
           GestureDetector(
             onTap: () {
-              showLogoutDialog(context);
+              showLogoutDialog(context, item);
             },
             child: Icon(
               Icons.delete,
@@ -104,7 +149,7 @@ class _UploadsState extends State<Uploads> {
     );
   }
 
-  void showLogoutDialog(BuildContext context) {
+  void showLogoutDialog(BuildContext context, Subject item) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -113,13 +158,15 @@ class _UploadsState extends State<Uploads> {
           content: Text('Do you want to DELETE?'),
           actions: [
             TextButton(
-              onPressed: () {},
+              onPressed: () async {
+                await contributionController.deleteYourUploads(
+                    itemId: item.id!);
+                Navigator.of(context).pop();
+              },
               child: Text('Yes'),
             ),
             TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
+              onPressed: () {},
               child: Text('No'),
             ),
           ],
